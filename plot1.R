75a5c166f766294ef38ad739ab94eddb2bb9e6e4
raho73 <ralf.hoenke@web.de>
2017-08-26 15:28:33 +0200

    1st

---
+++ plot1.R
@@ -0,0 +1,30 @@
+#set your working directory here or choose via menu, 
+setwd("C:/Users/ralf/Desktop/coursera/GitHUB/ExData_Plotting1/ExData_Plotting1")
+
+#load required packages
+#install.packages("data.table")
+#install.packages("dplyr")
+#install.packages("sqldf")
+#library(data.table)
+#library(dplyr)
+library(sqldf)
+
+###downloading/unzipping data from source
+src_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(src_url, file.path(getwd(), "src_data.zip"))
+unzip(zipfile = "src_data.zip")
+file.remove("src_data.zip")
+
+###import days 1/2/2002 and 2/2/2007 into data
+data <- read.csv.sql("household_power_consumption.txt", 
+                     sep = ';', 
+                     header = TRUE,
+                     sql="select * from file where Date in ('1/2/2007', '2/2/2007')")
+#dim(data)
+
+
+### hist
+png("plot1.png", width=480, height=480)
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
